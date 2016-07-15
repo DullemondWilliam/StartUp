618b95ec7032a094153cfcbebabe307fd0be4432
William Dullemond <Dullemond.William@gmail.com>
2016-07-15 13:19:39 -0400

    things

--- StartUpProgram/src/StartUpMain.java
+++ StartUpProgram/src/StartUpMain.java
@@ -1,39 +1,86 @@
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class StartUpMain {
 
 	public static void main(String[] args) {
-		isOnBattery();
-		
-		/*try{
-			Runtime runtime = Runtime.getRuntime();
-			Process process = runtime.exec("notePad");
-		}catch(Exception e){
+		Boolean battery = isOnBattery();
+
+		try {
+			Path path = FileSystems.getDefault().getPath("","settings");
+			InputStream in = Files.newInputStream(path);
+			BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+			String type = null;
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				if(line.contains("<>")){
+					type = reader.readLine();
+				}else if(type.equalsIgnoreCase("All Starting Commands")){
+					runCommand(line); 
+				}else if(type.equalsIgnoreCase("Plugged In Commands") && battery == false){
+					runCommand(line); 
+				}else if(type.equalsIgnoreCase("Battery Commands") && battery == true){
+					runCommand(line); 
+				}
+			}
+
+			reader.close();
+			in.close();
+		} catch (Exception e) {
 			e.printStackTrace();
-		}*/
+		}
 	}
+
+	private static Boolean runCommand(String command){
+		try {
+			Runtime runtime = Runtime.getRuntime();
+			Process process = runtime.exec(command);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+
+
 
 	private static Boolean isOnBattery(){
 		try{
+			Boolean out = false;
+
 			Runtime runtime = Runtime.getRuntime();
-			Process process = runtime.exec("powercfg /L");
-			BufferedReader stdInput = new BufferedReader(
-					new InputStreamReader(process.getInputStream()));
-			
-			String s;
-			while((s = stdInput.readLine()) != null){
-				if(s.contains("Balanced") && s.contains("*")){
-					System.out.println("We are on Battery Now");
+			Process process = runtime.exec("powercfg /batteryreport");
+
+			Path path = FileSystems.getDefault().getPath("","battery-report.html");
+			InputStream in = Files.newInputStream(path);
+			BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				// System.out.println(line);
+				if(line.contains("Report generated")){
+					reader.readLine();
+					reader.readLine();
+					reader.readLine();
+					if(reader.readLine().contains("Battery")){
+						out = true;
+					}
 				}
-				System.out.println(s);
 			}
-			
-			
+			reader.close();
+			in.close();
+			Files.delete(path);
+			return out;
 		}catch(Exception e){
 			e.printStackTrace();
 		}
 		return false;
 	}
-	
+
 }
